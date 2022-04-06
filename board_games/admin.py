6ca588d2fc289ba9d611c81e5ed090eefa2d502e
TheHees <joni.katila@outlook.com>
2022-04-06 15:40:06 +0300

    admin edits

--- board_games/admin.py
+++ board_games/admin.py
@@ -4,13 +4,11 @@ from .models import BoardGame, GameLoan
 
 # Register your models here.
 
-admin.site.register(BoardGame)
-admin.site.register(GameLoan)
 
-#@admin.site.register(BoardGame)
-#class GameAdmin(admin.ModelAdmin):
-#    list_display = ('Game', 'Genre', 'Year_published')
+@admin.register(BoardGame)
+class BoardGameAdmin(admin.ModelAdmin):
+    list_display = ('Game', 'Genre', 'Year_published')
 
-#@admin.site.register(GameLoan)
-#class LoanAdmin(admin.ModelAdmin):
-#    list_display = ('Game', 'Loaner', 'Date_loaned')
+@admin.register(GameLoan)
+class GameLoanAdmin(admin.ModelAdmin):
+    list_display = ('Game', 'Loaner', 'Date_loaned')
