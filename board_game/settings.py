e3fd621a91bddb17e6b2e3722fd4014547565ca8
Teemu Rintala <teemu.ma.rintala@outlook.com>
2022-04-20 19:01:13 +0300

    Adds Heroku setup

--- board_game/settings.py
+++ board_game/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # My settings
 LOGIN_URL = "/users/login/"
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
