e96658b206c9b00fc8d719b4f2b3905b55a23308
TheHees <joni.katila@outlook.com>
2022-04-20 17:25:04 +0300

    description of game

--- board_games/forms.py
+++ board_games/forms.py
@@ -6,7 +6,7 @@ from .models import BoardGame, GameLoan
 class GameForm(forms.ModelForm):
     class Meta:
         model = BoardGame
-        fields = ['game', 'genre', 'year_published']
+        fields = ['game', 'genre', 'year_published', 'description']
         labels = {'game': 'Game', 'genre': 'Genre', 'description': 'Description',
                   'year_published': 'Year Published'}
 
